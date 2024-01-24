3f0ca157234e90831196950f5825ca4571816a80
junting <10355098@st.thsh.tp.edu.tw>
2024-01-24 22:34:52 +0800

    Add background color and profile image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import profileImage from './img/生活照.jpg'
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <img src={profileImage} alt="profile-image" />
+      </body>
     </div>
   );
 }
